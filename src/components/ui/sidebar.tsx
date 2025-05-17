54dd518ea2c2b4e02a49f819624fc312f8b820a4
Royal-design <royaldesign1999@gmail.com>
2025-05-17 20:36:55 +0100

    updated

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps, cva } from "class-variance-authority";
-import { PanelLeftIcon } from "lucide-react";
+import { Menu } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
@@ -271,7 +271,9 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+      <div className="md:hidden">
+        <Menu strokeWidth={1} className="size-5" />
+      </div>
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
